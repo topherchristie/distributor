b4abf6e27ad5f17f304d4dec125a05b0d39f8b2b
Chris Christie <chris.christie@hs2solutions.com>
2016-06-17 14:04:06 -0500

    git ignore etc

--- app.js
+++ app.js
@@ -2,15 +2,45 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
+
+var dirName = process.argv[2];
+
+console.log('distributing', dirName);
+
+
 app.set('view engine', 'ejs');
 
-function getfiles(){
-  return fs.readdirSync(__dirname + '/dist');
+function getfiles(folder){
+  //return fs.readdirSync(dirName);
+  return getFilesRecursive(dirName + (folder?'/' + folder :''));
 }
+function getFilesRecursive (folder) {
+    var fileContents = fs.readdirSync(folder),
+        fileTree = [],
+        stats;
+
+    fileContents.forEach(function (fileName) {
+        stats = fs.lstatSync(folder + '/' + fileName);
+
+        if (stats.isDirectory()) {
+            fileTree.push({
+                name: fileName,
+                isDir:true,
+                children: getFilesRecursive(folder + '/' + fileName)
+            });
+        } else {
+            fileTree.push({
+                name: fileName
+            });
+        }
+    });
+
+    return fileTree;
+};
 
 var http = require('http').Server(app);
 
-app.use("/dist",express.static('dist'));
+app.use("/dist",express.static(dirName));
 
 // app.get('/',function(req,res) {
 // 	res.sendFile(__dirname + '/index.html');
@@ -19,10 +49,16 @@ app.use("/dist",express.static('dist'));
 app.get('/',function(req,res){
   var html = "<html><body><ul>";
 
-var logs = getfiles();
+var fld = (req.query.folder?req.query.folder + '/' :'');
+
+var logs = getfiles(req.query.folder);
 
 for(var i = 0;i<logs.length;i++){
-	html += "<li><a href='/dist/" + logs[i] + "'>" + logs[i] + "</a></li>";
+  if(logs[i].isDir){
+     html += "<li><a href='/?folder=" + fld + logs[i].name + "'>" + logs[i].name + "</a></li>";
+  }else{
+    	html += "<li><a href='/dist/" + fld + logs[i].name + "'>" + logs[i].name + "</a></li>";
+  }
 }
 
 
@@ -32,4 +68,19 @@ for(var i = 0;i<logs.length;i++){
 
 http.listen(3031, function() {
    console.log('listening *:3031');
+});
+
+var localtunnel = require('localtunnel');
+
+var tunnel = localtunnel(3031, {'subdomain':'efaredist'}, function(err, tunnel) {
+    if (err) throw err
+
+    // the assigned public url for your tunnel
+    // i.e. https://abcdefgjhij.localtunnel.me
+   console.log(tunnel.url);
+});
+
+tunnel.on('close', function() {
+    // tunnels are closed
+    console.log('tunnel closed');
 });
